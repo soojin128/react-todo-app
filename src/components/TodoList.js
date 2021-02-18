5a2980a01d82e62a6ad8bddaca96c8885d1facc2
Jenny Kim <kimsujin128@naver.com>
2021-02-18 14:15:57 +0900

    add local stotage

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,27 +1,35 @@
-import React, {useState} from 'react';
+import React, {
+  useState, useEffect
+} from 'react';
 import Todo from './Todo';
 import TodoForm from './TodoForm';
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
 
+  useEffect(() => {
+    const todoStore = JSON.parse(localStorage.getItem('todoStore'))
+    if(todoStore) setTodos(todoStore)
+  },[]);
+
+  useEffect(() => {
+    localStorage.setItem('todoStore', JSON.stringify(todos))
+  },[todos]);
+  
   const addTodo = todo => {
-    if(!todo.text || /^\s*$/.test(todo.text)){
+    if (!todo.text || /^\s*$/.test(todo.text)) {
       return;
     }
-
     const newTodos = [todo, ...todos];
-  
     setTodos(newTodos);
   }
 
-
   const updateTodo = (todoId, newValue) => {
-    if(!newValue.text || /^\s*$/.test(newValue.text)){
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
       return;
     }
-    
-    setTodos(prev => prev.map(item=>(item.id === todoId ? newValue : item)));
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
   }
 
 
@@ -31,22 +39,36 @@ function TodoList() {
     setTodos(removeArr);
   }
 
-
   const completeTodo = id => {
     let updatedTodos = todos.map(todo => {
-      if(todo.id === id) {
+      if (todo.id === id) {
         todo.isComplete = !todo.isComplete
       }
       return todo;
     });
     setTodos(updatedTodos);
   }
-
-  return (
-    <div>
-      <h1>What's the plan for Today?</h1>
-      <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/>
+  
+  return ( 
+  <div>
+    <h1 > What 's the plan for Today?</h1> 
+    <TodoForm onSubmit = {
+      addTodo
+    }
+    /> 
+    <Todo todos = {
+      todos
+    }
+    completeTodo = {
+      completeTodo
+    }
+    removeTodo = {
+      removeTodo
+    }
+    updateTodo = {
+      updateTodo
+    }
+    /> 
     </div>
   );
 }
